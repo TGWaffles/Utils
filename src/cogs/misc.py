d049241bfe0ea702194031b21df7d9d6ac9098ac
TGWaffles <thomas.wilcox@hotmail.co.uk>
2020-12-04 09:18:35 +0000

    Dev commit - Added quick round and swap to from seconds rather than from microseconds to total_latency.

--- src/cogs/misc.py
+++ src/cogs/misc.py
@@ -17,7 +17,7 @@ class Misc(commands.Cog):
         milliseconds_to_send = round((after - before).total_seconds() * 1000)
         message: discord.Message = ctx.message
         heartbeat_latency = round(self.bot.latency * 1000)
-        total_latency = (sent_message.created_at - message.created_at).microseconds // 1000
+        total_latency = round((sent_message.created_at - message.created_at).total_seconds() * 1000)
         embed = discord.Embed(title="Latency (Ping) Report")
         embed.add_field(name="Ping to Discord", value="{}ms".format(milliseconds_to_send // 2), inline=False)
         embed.add_field(name="Me -> Discord -> Me (Heartbeat)",
--- src/storage/config.py
+++ src/storage/config.py
@@ -1,4 +1,4 @@
-version_number = "1.1-dev02"
+version_number = "1.1-dev03"
 
 bot_prefix = "!"
 description = "Discord bot for Ahh-Monkey"
