7aaffb1ace805ff8e30221faf786854ddf97dcd6
TGWaffles <thomas.wilcox@hotmail.co.uk>
2020-12-04 11:20:59 +0000

    Add enable/disable command logic to the bot's list of extensions... oops

--- src/storage/config.py
+++ src/storage/config.py
@@ -1,10 +1,10 @@
-version_number = "1.2"
+version_number = "1.2.1"
 
 bot_prefix = "!"
 description = "Discord bot for Ahh-Monkey"
 owner_id = 230778630597246983
 
-extensions = ["suggestions", "restart", "audit", "purge", "misc"]
+extensions = ["suggestions", "restart", "audit", "purge", "misc", "manage_command"]
 
 guild_id = 725886999646437407
 error_channel_id = 784231099324301312
