6ec20ef3671b7b728ca00177f031632136009775
TGWaffles <thomas.wilcox@hotmail.co.uk>
2020-12-14 03:18:46 +0000

    chat spam preventer

---
+++ src/cogs/blacklist.py
@@ -0,0 +1,24 @@
+import datetime
+
+import discord
+from discord.ext import commands
+from src.storage import config
+from src.checks.role_check import is_staff
+from main import UtilsBot
+
+
+class Blacklist(commands.Cog):
+    def __init__(self, bot: UtilsBot):
+        self.bot: UtilsBot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if is_staff():
+            return
+
+        contents: str = message.contents
+        if "cantswim" in ''.join(filter(str.isalpha, contents)):
+            await message.delete()
--- src/storage/config.py
+++ src/storage/config.py
@@ -1,4 +1,4 @@
-version_number = "1.5.5"
+version_number = "1.5.6"
 
 bot_prefix = "!"
 description = "Discord bot for Ahh-Monkey"
