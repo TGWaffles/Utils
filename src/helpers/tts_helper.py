1f25aa25e3416b91f3f000c6a06c080c94777d49
TGWaffles <thomas.wilcox@hotmail.co.uk>
2021-01-03 23:14:56 +0000

    dev build

--- src/helpers/tts_helper.py
+++ src/helpers/tts_helper.py
@@ -8,9 +8,14 @@ from io import BytesIO
 def get_speak_file(message_content, lang):
     pre_processed = BytesIO()
     post_processed = BytesIO()
+    print(1)
     spoken_google = gTTS(message_content, lang=lang)
     spoken_google.write_to_fp(fp=pre_processed)
+    print(2)
     segment = pydub.AudioSegment.from_file(pre_processed, bitrate=356000, format="mp3")
+    print(3)
     segment = effects.speedup(segment, 1.25, 150, 25)
+    print(4)
     segment.set_frame_rate(16000).export(post_processed, format="wav")
+    print(5)
     return post_processed
--- src/storage/config.py
+++ src/storage/config.py
@@ -1,6 +1,6 @@
 import os
 
-version_number = "1.9.3"
+version_number = "1.9.3dev1"
 
 bot_prefix = "!"
 description = "Discord bot for Ahh-Monkey"
