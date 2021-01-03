64d91d3a364d9ac4f65a2129ccb7fbc667fa6d2d
TGWaffles <thomas.wilcox@hotmail.co.uk>
2021-01-03 23:38:36 +0000

    passing raw pcm audio now hopefully

--- src/cogs/text_to_speech.py
+++ src/cogs/text_to_speech.py
@@ -98,7 +98,7 @@ class TTS(commands.Cog):
         while voice_client.is_playing():
             await asyncio.sleep(0.1)
         try:
-            voice_client.play(discord.FFmpegPCMAudio(output, pipe=True))
+            voice_client.play(discord.PCMAudio(output))
         except discord.errors.ClientException:
             pass
         while voice_client.is_playing():
