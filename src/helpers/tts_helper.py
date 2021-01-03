2f75a23d03074f81418809621fad2c5860a27097
TGWaffles <thomas.wilcox@hotmail.co.uk>
2021-01-03 23:13:08 +0000

    specifying format mp3

--- src/helpers/tts_helper.py
+++ src/helpers/tts_helper.py
@@ -10,7 +10,7 @@ def get_speak_file(message_content, lang):
     post_processed = BytesIO()
     spoken_google = gTTS(message_content, lang=lang)
     spoken_google.write_to_fp(fp=pre_processed)
-    segment = pydub.AudioSegment.from_file(pre_processed, bitrate=356000)
+    segment = pydub.AudioSegment.from_file(pre_processed, bitrate=356000, format="mp3")
     segment = effects.speedup(segment, 1.25, 150, 25)
     segment.set_frame_rate(16000).export(post_processed, format="wav")
     return post_processed
