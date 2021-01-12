53c305113c54c4b87b1063c075c57e8261bc2161
TGWaffles <thomas.wilcox@hotmail.co.uk>
2021-01-12 02:28:04 +0000

    added await

--- src/cogs/purge.py
+++ src/cogs/purge.py
@@ -41,7 +41,7 @@ class Purge(commands.Cog):
                 await sent.edit(embed=self.bot.create_error_embed("This is a good thing. Crisis averted."))
         else:
             if amount > config.confirm_amount:
-                true_amount = [message for message in ctx.message.channel.history(limit=amount).flatten()
+                true_amount = [message for message in await ctx.message.channel.history(limit=amount).flatten()
                                if check(message)]
                 if true_amount < amount:
                     to_send = "{} of {}".format(true_amount, amount)
--- src/storage/config.py
+++ src/storage/config.py
@@ -1,6 +1,6 @@
 import os
 
-version_number = "1.15.4"
+version_number = "1.15.5"
 
 bot_prefix = "!"
 description = "Discord Utility Bot"
