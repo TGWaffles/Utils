d94b943be674be775ec2b0e0401252b773ad03bb
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-03-03 10:27:06 +0000

    dev build again, didnt work

--- src/helpers/graph_helper.py
+++ src/helpers/graph_helper.py
@@ -4,14 +4,19 @@ from io import BytesIO
 
 
 def file_from_timestamps(times, group):
+    print("process spawned.")
     file = BytesIO()
+    print("making series")
     series = pandas.Series(times)
     series.index = series.dt.to_period(group)
     series = series.groupby(level=0).size()
     series = series.reindex(pandas.period_range(series.index.min(), series.index.max(), freq=group), fill_value=0)
+    print("plotting graph")
     bar_chart = series.plot.bar(subplots=False)
     figure = bar_chart.get_figure()
     figure.tight_layout()
+    print("saving figure")
     figure.savefig(file)
     file.seek(0)
+    print("done")
     return file
