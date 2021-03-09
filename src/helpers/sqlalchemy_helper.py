0f64aa46236a6c8d62543d28b1cb2cffce1bbaca
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-03-09 20:01:03 +0000

    change to mysqlconnector

--- src/helpers/sqlalchemy_helper.py
+++ src/helpers/sqlalchemy_helper.py
@@ -34,8 +34,8 @@ from src.storage import config
 
 class DatabaseHelper:
     def __init__(self):
-        self.engine = sqlalchemy.create_engine("mysql+pymysql://utils:t93PRdtuyWgyt93PRdtuyWgy@elastic.thom.club/"
-                                               "utils?charset=utf8mb4")
+        self.engine = sqlalchemy.create_engine("mysql+mysqlconnector://utils:t93PRdtuyWgyt93PRdtuyWgy@elastic.thom"
+                                               ".club/utils?charset=utf8mb4")
         session_maker = sessionmaker(bind=self.engine)
         self.session_creator = scoped_session(session_maker)
         self.processing = Lock()
