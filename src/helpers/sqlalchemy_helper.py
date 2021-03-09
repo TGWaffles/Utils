2c63db5dcd9b48cddafab212c40131b9b16c7166
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-03-09 22:00:41 +0000

    added channels on full_guild

--- src/helpers/sqlalchemy_helper.py
+++ src/helpers/sqlalchemy_helper.py
@@ -325,6 +325,7 @@ class DatabaseHelper:
                 user_objects.pop(each.id)
             for each in session.query(Channel).filter(Channel.id.in_(channel_objects.keys())).all():
                 channel_objects.pop(each.id)
+            session.bulk_save_objects(channel_objects.values())
             session.bulk_save_objects(user_objects.values())
             session.bulk_save_objects(message_objects.values())
             print(len(message_objects))
