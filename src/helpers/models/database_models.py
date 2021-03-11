d5abbec867cbdbe54ae6fab5caa4133e93134897
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-03-11 17:46:08 +0000

    change how user is fetched from db

--- src/helpers/models/database_models.py
+++ src/helpers/models/database_models.py
@@ -210,7 +210,7 @@ class User(Base):
     def from_discord(cls, session, user: discord.User):
         with session.no_autoflush, user_lock:
             try:
-                user_object = session.query(User).filter_by(id=user.id).first()
+                user_object = session.query(User).get(id=user.id)
             except:
                 session.rollback()
                 return cls.from_discord(session, user)
