c891a889d77d779732b6cc62d3baec69f3ed7052
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-03-12 15:00:04 +0000

    fix for if original message is longer than 1024 too

--- src/cogs/db_api_client.py
+++ src/cogs/db_api_client.py
@@ -196,8 +196,12 @@ class DBApiClient(commands.Cog):
                         await sent.edit(embed=self.bot.create_error_embed("That message has no known edits."))
                         return
                     embed = discord.Embed(title="Edits for Message", colour=discord.Colour.gold())
+                    if len(edit.get("content")) > 1024:
+                        content = edit.get("content")[:1021] + "..."
+                    else:
+                        content = edit.get("content")
                     embed.add_field(name=f"Original Message ({original_timestamp_string})",
-                                    value=original_message.get("content"), inline=False)
+                                    value=content, inline=False)
                     for index, edit in enumerate(edits):
                         if len(embed) >= 5000:
                             break
