a1422a1a90ae06da280f2e479ece33aab0bc9c43
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-03-12 10:13:34 +0000

    removed console spam now I know it works

--- src/cogs/cat_guild.py
+++ src/cogs/cat_guild.py
@@ -14,36 +14,25 @@ class Cat(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
-        print("starting spotify...")
         if after.id == config.darby_id and after.guild.id == config.cat_guild_id:
             darby = after
         else:
             return
         music_channel = self.bot.get_channel(config.darby_channel_id)
-        print("got channel")
-        print(darby)
-        print(darby.activities)
         if darby.activities:
-            print("she has activities!")
             for activity in darby.activities:
-                print(type(activity))
                 if isinstance(activity, discord.Spotify):
-                    print("spotify one!")
                     if activity.title != self.last_title:
-                        print("not the same as last time.")
                         embed = discord.Embed(
                             title=f"{darby.name}'s Spotify",
                             description="Listening to {}".format(activity.title),
-                            color=0xC902FF)
+                            color=discord.Colour.green())
                         self.last_title = activity.title
-                        print("set stuff up")
                         embed.set_thumbnail(url=activity.album_cover_url)
                         embed.add_field(name="Artist", value=activity.artist)
                         embed.add_field(name="Album", value=activity.album)
                         embed.set_footer(text="Song started at {}".format(activity.created_at.strftime("%H:%M")))
-                        print("sending...")
                         await music_channel.send(embed=embed)
-                        print("Sent!")
 
 
 def setup(bot):
