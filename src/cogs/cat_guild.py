778c2e33adde96a6d6327304e403127c2957790c
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-03-12 10:03:18 +0000

    debug rq

--- src/cogs/cat_guild.py
+++ src/cogs/cat_guild.py
@@ -15,23 +15,32 @@ class Cat(commands.Cog):
 
     @tasks.loop(seconds=5, count=None)
     async def update_spotify_embed(self):
+        print("starting spotify...")
         cat_guild = self.bot.get_guild(config.cat_guild_id)
         darby: discord.Member = await cat_guild.fetch_member(config.darby_id)
         music_channel = self.bot.get_channel(config.darby_channel_id)
+        print("got channel")
         if darby.activities:
+            print("she has activities!")
             for activity in darby.activities:
+                print(type(activity))
                 if isinstance(activity, discord.Spotify):
+                    print("spotify one!")
                     if activity.title != self.last_title:
+                        print("not the same as last time.")
                         embed = discord.Embed(
                             title=f"{darby.name}'s Spotify",
                             description="Listening to {}".format(activity.title),
                             color=0xC902FF)
                         self.last_title = activity.title
+                        print("set stuff up")
                         embed.set_thumbnail(url=activity.album_cover_url)
                         embed.add_field(name="Artist", value=activity.artist)
                         embed.add_field(name="Album", value=activity.album)
                         embed.set_footer(text="Song started at {}".format(activity.created_at.strftime("%H:%M")))
+                        print("sending...")
                         await music_channel.send(embed=embed)
+                        print("Sent!")
 
 
 def setup(bot):
