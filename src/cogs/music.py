14eda9973eba31aed11c4320c5a9a8ccfaad940c
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-04-21 02:00:17 +0100

    debug new search

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -82,9 +82,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
         if search:
             query = youtube_search.CustomSearch(url, youtube_search.VideoSortOrder.relevance, limit=1)
             data = await query.next()
-            print(data)
-            print(data.get("result"))
-            return data.get("result")[0].get("link")
+            return json.loads(data.get("result")[0]).get("link")
         else:
             try:
                 attempts = 0
