290b3e7a4c831900757983d1fcd06f7d9a5a093f
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-04-21 01:30:56 +0100

    more logging info i think

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -37,8 +37,8 @@ ytdl_format_options = {
     'noplaylist': True,
     'nocheckcertificate': True,
     'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
+    'logtostderr': True,
+    'quiet': False,
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
