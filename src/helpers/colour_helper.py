c108bdd800e9d97774ed0db2a646cb891dd6328c
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-05-10 13:27:13 +0100

    Add imports to colour helper

--- src/helpers/colour_helper.py
+++ src/helpers/colour_helper.py
@@ -1,3 +1,8 @@
+import discord
+import webcolors
+from discord.ext import commands
+
+
 def convert_colour(input_colour):
     input_colour = input_colour.strip('#')
     try:
@@ -8,9 +13,9 @@ def convert_colour(input_colour):
         if len(colour) == 6:
             return discord.Colour.from_rgb(int(colour[:2], 16), int(colour[2:4], 16), int(colour[4:6], 16))
         else:
-            raise commands.BadArgument
+            raise commands.BadArgument()
     except ValueError:
         try:
             return discord.Colour.from_rgb(*(webcolors.name_to_rgb(input_colour.replace(" ", ""))))
         except ValueError:
-            raise commands.BadArgument
+            raise commands.BadArgument()
