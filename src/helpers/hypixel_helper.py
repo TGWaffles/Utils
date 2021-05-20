aa873a74d50e40eecabf70f8fdaf8fe0f78539ad
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-05-20 15:20:09 +0100

    print out whatever it figured out ig

--- src/helpers/hypixel_helper.py
+++ src/helpers/hypixel_helper.py
@@ -299,6 +299,7 @@ def extrapolate_threat_index(input_threat_indexes: list[int], amount):
 
     params, _ = curve_fit(quadratic_fit, list(range(len(input_threat_indexes))), input_threat_indexes)
     a, b, c = params
+    print(params)
     if a != 0:
         def func_in_terms_of_y(y):
             return ((-b + (((b ** 2) - (4 * a * c) + (4 * a * y)) ** (1 / 2))) / (2 * a),
