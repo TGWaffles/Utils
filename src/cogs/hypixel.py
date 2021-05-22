9a6549efecfdad62b5bd81d6f764dd4b897de258
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-05-22 19:19:26 +0100

    accidentally used from_stats rather than from_dict

--- src/cogs/hypixel.py
+++ src/cogs/hypixel.py
@@ -687,12 +687,8 @@ class Hypixel(commands.Cog):
             if first_document == last_document:
                 await ctx.reply(embed=self.bot.create_error_embed(f"I've only recorded one data point for {username}."))
                 return
-            print(first_document)
-            print(last_document)
-            latest_stats = HypixelStats.from_stats(last_document["stats"])
-            earliest_stats = HypixelStats.from_stats(first_document["stats"])
-            print(latest_stats.kills)
-            print(earliest_stats.kills)
+            latest_stats = HypixelStats.from_dict(last_document["stats"])
+            earliest_stats = HypixelStats.from_dict(first_document["stats"])
             all_embeds = create_delta_embeds(f"{username}'s Stats - All Recorded", earliest_stats, latest_stats)
             paginator = EmbedPaginator(self.bot, None, all_embeds, ctx)
             await paginator.start()
