12e8dd4f4e040fccc8563ef59bb8b7866c1a03cb
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-06-27 21:55:31 +0100

    add book minimum and average

--- src/cogs/skyblock.py
+++ src/cogs/skyblock.py
@@ -31,22 +31,25 @@ class Skyblock(commands.Cog):
                                                               "Please specify a subcommand. Valid "
                                                               "subcommands: `history`, `average`, `minimum`"))
 
+    async def get_item_data(self, query, book=False):
+        minimum_prices = []
+        average_prices = []
+        maximum_prices = []
+        for timestamp, all_auctions in await self.get_bin_auctions(query, book=book):
+            gc.collect()
+            known_auctions = [x.get("starting_bid") for x in all_auctions]
+            if len(known_auctions) == 0:
+                continue
+            minimum_prices.append((timestamp, min(known_auctions)))
+            average_prices.append((timestamp, mean(known_auctions)))
+            maximum_prices.append((timestamp, max(known_auctions)))
+        return minimum_prices, average_prices, maximum_prices
+
     @book.command(name="history")
     async def book_history(self, ctx, *, query):
         query = query.lower()
         async with ctx.typing():
-            minimum_prices = []
-            average_prices = []
-            maximum_prices = []
-            for timestamp, all_auctions in await self.get_bin_auctions(query, book=True):
-                gc.collect()
-                known_auctions = [x.get("starting_bid") for x in all_auctions]
-                if len(known_auctions) == 0:
-                    print("no known auctions")
-                    continue
-                minimum_prices.append((timestamp, min(known_auctions)))
-                average_prices.append((timestamp, mean(known_auctions)))
-                maximum_prices.append((timestamp, max(known_auctions)))
+            minimum_prices, average_prices, maximum_prices = await self.get_item_data(query, True)
             if len(maximum_prices) == 0:
                 await ctx.reply(embed=self.bot.create_error_embed("No auctions could be found."))
                 return
@@ -58,6 +61,45 @@ class Skyblock(commands.Cog):
                                                                          Minimum=minimum_prices,
                                                                          Average=average_prices,
                                                                          Maximum=maximum_prices))
+            file = BytesIO(data)
+            file.seek(0)
+            discord_file = discord.File(fp=file, filename="image.png")
+            await ctx.reply(file=discord_file)
+
+    @book.command(name="average")
+    async def book_average(self, ctx, *, query):
+        query = query.lower()
+        async with ctx.typing():
+            minimum_prices, average_prices, maximum_prices = await self.get_item_data(query, True)
+            if len(maximum_prices) == 0:
+                await ctx.reply(embed=self.bot.create_error_embed("No auctions could be found."))
+                return
+            with ProcessPoolExecutor() as pool:
+                data = await self.bot.loop.run_in_executor(pool, partial(plot_multiple,
+                                                                         title=f"Average prices for {query} books",
+                                                                         x_label="Date",
+                                                                         y_label="Price in coins",
+                                                                         Minimum=minimum_prices,
+                                                                         Average=average_prices))
+            file = BytesIO(data)
+            file.seek(0)
+            discord_file = discord.File(fp=file, filename="image.png")
+            await ctx.reply(file=discord_file)
+
+    @book.command(name="minimum")
+    async def book_average(self, ctx, *, query):
+        query = query.lower()
+        async with ctx.typing():
+            minimum_prices, average_prices, maximum_prices = await self.get_item_data(query, True)
+            if len(maximum_prices) == 0:
+                await ctx.reply(embed=self.bot.create_error_embed("No auctions could be found."))
+                return
+            with ProcessPoolExecutor() as pool:
+                data = await self.bot.loop.run_in_executor(pool, partial(plot_multiple,
+                                                                         title=f"Minimum prices for {query} books",
+                                                                         x_label="Date",
+                                                                         y_label="Price in coins",
+                                                                         Minimum=minimum_prices))
             file = BytesIO(data)
             file.seek(0)
             discord_file = discord.File(fp=file, filename="image.png")
@@ -106,7 +148,6 @@ class Skyblock(commands.Cog):
                     }
                 }
             }
-            print(final_match)
         pipeline.append(final_match)
         auctions = await self.skyblock_db.auction_pages.aggregate(pipeline=pipeline).to_list(length=None)
         return auction["timestamp"], auctions
@@ -130,17 +171,10 @@ class Skyblock(commands.Cog):
     @skyblock.command()
     async def history(self, ctx, *, query):
         async with ctx.typing():
-            minimum_prices = []
-            average_prices = []
-            maximum_prices = []
-            for timestamp, all_auctions in await self.get_bin_auctions(query.lower()):
-                gc.collect()
-                known_auctions = [x.get("starting_bid") for x in all_auctions]
-                if len(known_auctions) == 0:
-                    continue
-                minimum_prices.append((timestamp, min(known_auctions)))
-                average_prices.append((timestamp, mean(known_auctions)))
-                maximum_prices.append((timestamp, max(known_auctions)))
+            minimum_prices, average_prices, maximum_prices = await self.get_item_data(query, False)
+            if len(maximum_prices) == 0:
+                await ctx.reply(embed=self.bot.create_error_embed("No auctions could be found."))
+                return
             with ProcessPoolExecutor() as pool:
                 data = await self.bot.loop.run_in_executor(pool, partial(plot_multiple,
                                                                          title=f"Prices for {query}",
@@ -157,15 +191,10 @@ class Skyblock(commands.Cog):
     @skyblock.command()
     async def average(self, ctx, *, query):
         async with ctx.typing():
-            minimum_prices = []
-            average_prices = []
-            for timestamp, all_auctions in await self.get_bin_auctions(query.lower()):
-                gc.collect()
-                known_auctions = [x.get("starting_bid") for x in all_auctions]
-                if len(known_auctions) == 0:
-                    continue
-                minimum_prices.append((timestamp, min(known_auctions)))
-                average_prices.append((timestamp, mean(known_auctions)))
+            minimum_prices, average_prices, maximum_prices = await self.get_item_data(query, False)
+            if len(maximum_prices) == 0:
+                await ctx.reply(embed=self.bot.create_error_embed("No auctions could be found."))
+                return
             with ProcessPoolExecutor() as pool:
                 data = await self.bot.loop.run_in_executor(pool, partial(plot_multiple,
                                                                          title=f"Average prices for {query}",
@@ -181,13 +210,10 @@ class Skyblock(commands.Cog):
     @skyblock.command()
     async def minimum(self, ctx, *, query):
         async with ctx.typing():
-            minimum_prices = []
-            for timestamp, all_auctions in await self.get_bin_auctions(query.lower()):
-                gc.collect()
-                known_auctions = [x.get("starting_bid") for x in all_auctions]
-                if len(known_auctions) == 0:
-                    continue
-                minimum_prices.append((timestamp, min(known_auctions)))
+            minimum_prices, average_prices, maximum_prices = await self.get_item_data(query, False)
+            if len(maximum_prices) == 0:
+                await ctx.reply(embed=self.bot.create_error_embed("No auctions could be found."))
+                return
             with ProcessPoolExecutor() as pool:
                 data = await self.bot.loop.run_in_executor(pool, partial(plot_multiple,
                                                                          title=f"Minimum prices for {query}",
