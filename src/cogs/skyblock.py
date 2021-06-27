10bf3a8fdd7beb99c9f8102fbf32ed8f241c81b3
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-06-27 21:36:25 +0100

    query has spaces and books use mongo

--- src/cogs/skyblock.py
+++ src/cogs/skyblock.py
@@ -32,15 +32,15 @@ class Skyblock(commands.Cog):
                                                               "subcommands: `history`, `average`, `minimum`"))
 
     @book.command(name="history")
-    async def book_history(self, ctx, query):
+    async def book_history(self, ctx, *, query):
         query = query.lower()
         async with ctx.typing():
             minimum_prices = []
             average_prices = []
             maximum_prices = []
-            for timestamp, all_auctions in await self.get_bin_auctions("enchanted book"):
+            for timestamp, all_auctions in await self.get_bin_auctions(query, book=True):
                 gc.collect()
-                known_auctions = [x.get("starting_bid") for x in all_auctions if query in x.get("item_lore").lower()]
+                known_auctions = [x.get("starting_bid") for x in all_auctions]
                 minimum_prices.append((timestamp, min(known_auctions)))
                 average_prices.append((timestamp, mean(known_auctions)))
                 maximum_prices.append((timestamp, max(known_auctions)))
@@ -57,7 +57,7 @@ class Skyblock(commands.Cog):
             discord_file = discord.File(fp=file, filename="image.png")
             await ctx.reply(file=discord_file)
 
-    async def auctions_from_parent(self, auction, query):
+    async def auctions_from_parent(self, auction, query, item_lore=None):
         pipeline = [
             {
                 "$match": {
@@ -75,32 +75,53 @@ class Skyblock(commands.Cog):
             },
             {
                 "$replaceWith": "$auctions"
-            },
-            {
+            }
+        ]
+        final_match = {
+            "$match": {
+                "bin": True,
+                "item_name": {
+                    "$regex": f".*{query}.*",
+                    "$options": 'i'
+                }
+            }
+        }
+        if item_lore is not None:
+            final_match = {
                 "$match": {
                     "bin": True,
                     "item_name": {
                         "$regex": f".*{query}.*",
                         "$options": 'i'
+                    },
+                    "item_lore": {
+                        "$regex": f".*{item_lore}.*",
+                        "$options": 'i'
                     }
                 }
             }
-        ]
+        pipeline.append(final_match)
         auctions = await self.skyblock_db.auction_pages.aggregate(pipeline=pipeline).to_list(length=None)
         return auction["timestamp"], auctions
 
-    async def get_bin_auctions(self, query):
+    async def get_bin_auctions(self, query, book=False):
         coroutines = []
         futures = []
-        async for auction in self.skyblock_db.auctions.find().sort("timestamp", 1):
-            coroutines.append(self.auctions_from_parent(auction, query))
+        if book:
+            query = "enchanted book"
+            lore_query = query
+            async for auction in self.skyblock_db.auctions.find().sort("timestamp", 1):
+                coroutines.append(self.auctions_from_parent(auction, query, item_lore=lore_query))
+        else:
+            async for auction in self.skyblock_db.auctions.find().sort("timestamp", 1):
+                coroutines.append(self.auctions_from_parent(auction, query))
         for coroutine in coroutines:
             futures.append(self.bot.loop.create_task(coroutine))
         results = await asyncio.gather(*futures)
         return results
 
     @skyblock.command()
-    async def history(self, ctx, query):
+    async def history(self, ctx, *, query):
         async with ctx.typing():
             minimum_prices = []
             average_prices = []
@@ -125,7 +146,7 @@ class Skyblock(commands.Cog):
             await ctx.reply(file=discord_file)
 
     @skyblock.command()
-    async def average(self, ctx, query):
+    async def average(self, ctx, *, query):
         async with ctx.typing():
             minimum_prices = []
             average_prices = []
@@ -147,7 +168,7 @@ class Skyblock(commands.Cog):
             await ctx.reply(file=discord_file)
 
     @skyblock.command()
-    async def minimum(self, ctx, query):
+    async def minimum(self, ctx, *, query):
         async with ctx.typing():
             minimum_prices = []
             for timestamp, all_auctions in await self.get_bin_auctions(query.lower()):
