17852a886eb8dbd5059444f928db5b0ca47cf5fb
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-07-06 13:24:48 +0100

    debug wrapped in list

--- src/helpers/graph_helper.py
+++ src/helpers/graph_helper.py
@@ -46,7 +46,7 @@ def plot_multiple(x_label="", y_label="", title="", **kwargs):
     file = BytesIO()
     plt.gca().xaxis.set_major_formatter(dates.DateFormatter("%Y-%m-%d %H:%M"))
     plt.gca().yaxis.set_major_formatter(num_humanizer)
-    print(max(x) for x in kwargs.values())
+    print([max(x) for x in kwargs.values()])
     interval = max([1] + [(max(x) - min(x)).total_seconds() // 600 for x in kwargs.values()])
     print(interval)
     plt.gca().xaxis.set_major_locator(dates.HourLocator(interval=interval))
