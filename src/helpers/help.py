df8623242f923b2333a41714affff5bc15e422e2
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-07-08 21:29:49 +0100

    dont do nonetype

--- src/helpers/help.py
+++ src/helpers/help.py
@@ -6,7 +6,8 @@ from src.helpers.paginator import Paginator
 class UtilsHelp(MinimalHelpCommand):
     def __init__(self, bot, **options):
         super().__init__(**options)
-        self.paginator = Paginator(bot=bot, channel=self.get_destination())
+        self.paginator = Paginator(bot=bot, channel=None)
 
     async def send_pages(self):
+        self.paginator.channel = self.get_destination()
         await self.paginator.start()
