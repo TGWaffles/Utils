055797bf574f0787c4c86111533f842839e29a9c
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2021-07-09 18:19:26 +0100

    debug

--- src/helpers/help.py
+++ src/helpers/help.py
@@ -5,6 +5,7 @@ from src.helpers.paginator import Paginator
 
 class UtilsHelp(MinimalHelpCommand):
     def __init__(self, bot, **options):
+        print(options)
         super().__init__(**options)
         self.paginator = Paginator(bot=bot, channel=None)
 
